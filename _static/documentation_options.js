3025d844efd38e3a6f40ac2f42c5d4db5b49f3a2
michaelmarty <martymt10@gmail.com>
2024-01-05 13:46:02 -0700

    Version 7 Doc Draft

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,5 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '6.0.0.b2',
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '7.0.0.b1',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
